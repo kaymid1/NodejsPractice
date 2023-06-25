f251bf7df90fa47b91e70e717ed78262ef1f1a99
kaymid <dquanht0@gmail.com>
2023-06-26 00:03:02 +0700

    modify cors.js, add put&del, courseId api

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Courses = require("../models/courses");
+const cors = require("./cors");
 const courseRouter = express.Router();
 var authenticate = require("../authenticate");
 courseRouter
   .route("/")
-  .get((req, res, next) => {
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
     Courses.find({})
       .populate("review.author")
       .then(
@@ -18,13 +22,62 @@ courseRouter
       )
       .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    console.log("Post function processing");
-    console.log(req.body);
-    Courses.create(req.body)
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      console.log("Post function processing");
+      console.log(req.body);
+      Courses.create(req.body)
+        .then(
+          (course) => {
+            console.log("Course created", course);
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(course);
+          },
+          (err) => next(err)
+        )
+        .catch((err) => next(err));
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end("PUT operation not supported on /courses");
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Courses.deleteMany({})
+        .then(
+          (resp) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(resp);
+          },
+          (err) => next(err)
+        )
+        .catch((err) => next(err));
+    }
+  );
+courseRouter
+  .route("/:courseId")
+  .options(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
+    Courses.findById(req.params.courseId)
+      .populate("review.author")
       .then(
         (course) => {
-          console.log("Course created", course);
           res.statusCode = 200;
           res.setHeader("Content-Type", "application/json");
           res.json(course);
@@ -32,5 +85,57 @@ courseRouter
         (err) => next(err)
       )
       .catch((err) => next(err));
-  });
+  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end(
+        "POST operation not supported on /courses/" + req.params.courseId
+      );
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Courses.findByIdAndUpdate(
+        req.params.courseId,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      )
+        .then(
+          (dish) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(dish);
+          },
+          (err) => next(err)
+        )
+        .catch((err) => next(err));
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Courses.findOneAndRemove(req.params.courseId)
+        .then(
+          (resp) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(resp);
+          },
+          (err) => next(err)
+        )
+        .catch((err) => next(err));
+    }
+  );
+
 module.exports = courseRouter;

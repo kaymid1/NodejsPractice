da3096d313114718cf4091701783463ffdbc02ed
kaymid <dquanht0@gmail.com>
2023-06-25 18:11:54 +0700

    implement cors

--- routes/leadersRouter.js
+++ routes/leadersRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const leadersRouter = express.Router();
 var authenticate = require("../authenticate");
+const cors = require("./cors");
 leadersRouter.use(bodyParser.json());
 
 leadersRouter
@@ -11,48 +12,83 @@ leadersRouter
     res.setHeader("Content-Type", "text/plain");
     next();
   })
-  .get((req, res, next) => {
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
     res.end("Will send all the leaders to you!");
   })
 
-  .post(authenticate.verifyUser, (req, res, next) => {
-    res.end(
-      "Will add the leader: " +
-        req.body.name +
-        " with details: " +
-        req.body.description
-    );
-  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.end(
+        "Will add the leader: " +
+          req.body.name +
+          " with details: " +
+          req.body.description
+      );
+    }
+  )
 
-  .put(authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /leaders");
-  })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    res.end("Deleting all leaders");
-  });
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end("PUT operation not supported on /leaders");
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.end("Deleting all leaders");
+    }
+  );
 leadersRouter
   .route("/:leaderId")
-  .get((req, res, next) => {
+  .get(cors.cors, (req, res, next) => {
     res.end(
       "Will send details of the leader: " + req.params.leaderId + " to you!"
     );
   })
-  .post(authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.end("POST operation not supported on /leaders/" + req.params.leaderId);
-  })
-  .put(authenticate.verifyUser, (req, res, next) => {
-    res.write("Updating the leader: " + req.params.leaderId + "\n");
-    res.end(
-      "Will update the leader: " +
-        req.body.name +
-        " with details: " +
-        req.body.description
-    );
-  })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    res.end("Deleting leader: " + req.params.leaderId);
-  });
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end(
+        "POST operation not supported on /leaders/" + req.params.leaderId
+      );
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.write("Updating the leader: " + req.params.leaderId + "\n");
+      res.end(
+        "Will update the leader: " +
+          req.body.name +
+          " with details: " +
+          req.body.description
+      );
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.end("Deleting leader: " + req.params.leaderId);
+    }
+  );
 
 module.exports = leadersRouter;
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const promoRouter = express.Router();
 var authenticate = require("../authenticate");
+const cors = require("./cors");
 promoRouter.use(bodyParser.json());
 
 promoRouter
@@ -11,49 +12,85 @@ promoRouter
     res.setHeader("Content-Type", "text/plain");
     next();
   })
-  .get((req, res, next) => {
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
     res.end("Will send all the promotions to you!");
   })
 
-  .post(authenticate.verifyUser, (req, res, next) => {
-    res.end(
-      "Will add the promotions: " +
-        req.body.name +
-        " with details: " +
-        req.body.description
-    );
-  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.end(
+        "Will add the promotions: " +
+          req.body.name +
+          " with details: " +
+          req.body.description
+      );
+    }
+  )
 
-  .put(authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /promotions");
-  })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    res.end("Deleting all promotions");
-  });
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end("PUT operation not supported on /promotions");
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.end("Deleting all promotions");
+    }
+  );
 promoRouter
   .route("/:promoId")
-  .get((req, res, next) => {
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
     res.end(
       "Will send details of the promotions: " + req.params.promoId + " to you!"
     );
   })
-  .post(authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.end(
-      "POST operation not supported on /promotions/" + req.params.promoId
-    );
-  })
-  .put(authenticate.verifyUser, (req, res, next) => {
-    res.write("Updating the promotions: " + req.params.promoId + "\n");
-    res.end(
-      "Will update the promtions: " +
-        req.body.name +
-        " with details: " +
-        req.body.description
-    );
-  })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    res.end("Deleting promotions: " + req.params.promoId);
-  });
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end(
+        "POST operation not supported on /promotions/" + req.params.promoId
+      );
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.write("Updating the promotions: " + req.params.promoId + "\n");
+      res.end(
+        "Will update the promtions: " +
+          req.body.name +
+          " with details: " +
+          req.body.description
+      );
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.end("Deleting promotions: " + req.params.promoId);
+    }
+  );
 module.exports = promoRouter;

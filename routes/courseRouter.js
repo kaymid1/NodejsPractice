3283af6c9d719cd782a95c301829cb9b2a51e2c1
kaymid <dquanht0@gmail.com>
2023-06-26 00:24:50 +0700

    courseId/reviews operation ->restapi

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -137,5 +137,105 @@ courseRouter
         .catch((err) => next(err));
     }
   );
-
+courseRouter
+  .route("/:courseId/reviews")
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
+    Courses.findById(req.params.courseId)
+      .populate("review.author")
+      .then(
+        (course) => {
+          if (course != null) {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(course.reviews);
+          } else {
+            err = new Error("Course " + req.params.dishId + "not found");
+            err.status = 404;
+            return next(err);
+          }
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Courses.findById(req.params.courseId)
+        .then(
+          (course) => {
+            if (dish != null) {
+              course.reviews.push(req.body);
+              course.save().then(
+                (course) => {
+                  Courses.findById(course._id)
+                    .populate("review.author")
+                    .then((course) => {
+                      res.statusCode = 200;
+                      res.setHeader("Content-Type", "application/json");
+                      res.json(course);
+                    });
+                },
+                (err) => next(err)
+              );
+            } else {
+              err = new Error("Course " + req.params.dishId + "not found");
+              err.status = 404;
+              return next(err);
+            }
+          },
+          (err) => next(err)
+        )
+        .catch((err) => next(err));
+    }
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 200;
+      res.end(
+        "PUT operation not supported on /courses/" +
+          req.params.courseId +
+          "/reviews"
+      );
+    }
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Courses.findById(req.params.courseId)
+        .then(
+          (course) => {
+            if (course != null) {
+              for (var i = course.reviews.length - 1; i >= 0; i--) {
+                course.reviews.id(course.reviews[i]._id).remove();
+              }
+              course.save().then(
+                (course) => {
+                  res.statusCode = 200;
+                  res.setHeader("Content-Type", "application/json");
+                  res.json(course);
+                },
+                (err) => next(err)
+              );
+            } else {
+              err = new Error("Course " + req.params.courseId + " not found");
+              err.status = 404;
+              return next(err);
+            }
+          },
+          (err) => next(err)
+        )
+        .catch((err) => next(err));
+    }
+  );
 module.exports = courseRouter;
